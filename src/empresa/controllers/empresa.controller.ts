67618c987505bfe62d16a0e73d83d1b69f96106e
Matheus <matheushnrk@outlook.com>
2025-10-02 15:23:02 -0300

    Matheus: Consultar empresa por ID, consultar empresa por nome, cadastrar empresa.

--- src/empresa/controllers/empresa.controller.ts
+++ src/empresa/controllers/empresa.controller.ts
@@ -1,15 +1,32 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable prettier/prettier */
-import { Controller, Get, HttpCode, HttpStatus } from "@nestjs/common";
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post } from "@nestjs/common";
 import { EmpresaService } from "../services/empresa.service";
 import { Empresa } from "../entities/empresa.entity";
 
 @Controller("/empresas")
 export class EmpresaController {
-    constructor(private readonly empresaService: EmpresaService) { }
+  constructor(private readonly empresaService: EmpresaService) {}
 
-    @Get()
-    @HttpCode(HttpStatus.OK)
-    findAll(): Promise<Empresa[]>{
-        return this.empresaService.findAll();
-    }
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  findAll(): Promise<Empresa[]> {
+    return this.empresaService.findAll();
+  }
+
+  @Get('/:id')
+  @HttpCode(HttpStatus.OK)
+  findById(@Param('id') id: number): Promise<Empresa> {
+    return this.empresaService.findById(id);
+  }
+  @Get('/nome_empresa/:nome_empresa')
+  @HttpCode(HttpStatus.OK)
+  findAllByNome_Empresa(@Param('nome_empresa') nome_empresa: string): Promise<Empresa[]> {
+    return this.empresaService.findAllByNome_Empresa(nome_empresa);
+  }
+@Post()
+@HttpCode(HttpStatus.CREATED)
+create(@Body() empresa: Empresa): Promise<Empresa> {
+  return this.empresaService.create(empresa);
+}
 }
--- src/empresa/services/empresa.service.ts
+++ src/empresa/services/empresa.service.ts
@@ -1,18 +1,41 @@
 /* eslint-disable prettier/prettier */
 import { InjectRepository } from "@nestjs/typeorm";
 import { Empresa } from "../entities/empresa.entity";
-import { Repository } from "typeorm";
-import { Injectable } from "@nestjs/common";
+import { ILike, Repository } from "typeorm";
+import { Injectable, HttpException, HttpStatus } from "@nestjs/common";
 
 @Injectable()
 export class EmpresaService {
-    constructor(
-        @InjectRepository(Empresa)
-    private empresaReposiory: Repository<Empresa>
-    ) { }
+  constructor(
+    @InjectRepository(Empresa)
+    private readonly empresaRepository: Repository<Empresa>,
+  ) {}
 
-    async findAll(): Promise<Empresa[]> {
-        return await this.empresaReposiory.find();
+  async findAll(): Promise<Empresa[]> {
+    return await this.empresaRepository.find();
+  }
+
+  async findById(id: number): Promise<Empresa> {
+    const empresa = await this.empresaRepository.findOne({
+      where: { id },
+    });
+
+    if (!empresa) {
+      throw new HttpException('Empresa não encontrada', HttpStatus.NOT_FOUND);
     }
-    
+
+    return empresa;
+  }
+
+  async findAllByNome_Empresa(nome_empresa: string): Promise<Empresa[]> {
+    return await this.empresaRepository.find({
+      where: { 
+        nome_empresa: ILike(`%${nome_empresa}%`)
+
+       },
+    });
+  }
+async create(empresa: Empresa): Promise<Empresa> {
+    return await this.empresaRepository.save(empresa);
+  }
 }
