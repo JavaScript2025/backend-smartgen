6a14e18532bd42907ce17fc41b6c1bbd66709628
inglyd <inglydalves@outlook.com>
2025-10-03 10:37:22 -0300

    completa metodos no controller

--- src/usuario/controllers/usuario.controller.ts
+++ src/usuario/controllers/usuario.controller.ts
@@ -1,15 +1,44 @@
-import { Controller, Get, HttpCode, HttpStatus } from "@nestjs/common";
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put } from "@nestjs/common";
 import { Usuario } from "../entities/usuario.entity";
 import { UsuarioService } from "../services/usuario.service";
 
 @Controller("/usuarios")
 export class UsuarioController {
     constructor(private readonly usuarioService: UsuarioService) {}
-
+   
     @Get()
     @HttpCode(HttpStatus.OK)
     findAll(): Promise<Usuario[]> {
         return this.usuarioService.findAll();
+    }
+
+    @Get('/:id')
+    @HttpCode(HttpStatus.OK)
+    FindById(@Param('id', ParseIntPipe) id: number): Promise<Usuario> {
+        return this.usuarioService.findById(id);
+    }
+
+    @Get('buscar/:usuario')
+    async findByUsuario(@Param('usuario') usuario: string): Promise<Usuario | null> {
+      return await this.usuarioService.findByUsuario(usuario);
+    }
+
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    create(@Body() usuario: Usuario): Promise<Usuario> {
+        return this.usuarioService.create(usuario);
+    }
+
+    @Put()
+    @HttpCode(HttpStatus.OK)
+    update(@Body() usuario: Usuario): Promise<Usuario> {
+        return this.usuarioService.update(usuario);
+    }
+
+    @Delete('/:id')
+    @HttpCode(HttpStatus.NO_CONTENT)
+    delete(@Param('id', ParseIntPipe) id: number){
+        return this.usuarioService.delete(id);
     }
 
 }
